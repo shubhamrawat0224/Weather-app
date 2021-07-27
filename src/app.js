8daa7cb238667907f5aca2f7330ac6f20a53d9a4
shubham rawat <shubhamrawat0224@gmail.com>
2021-07-27 17:32:16 +0530

    app for heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs')
 const geocode =require('./utils/geocode')
 const forecast=require('./utils/forecast')
 const app = express()
+const port = process.env.PORT || 3000
 // Define paths for Express config
 const publicDiretory=path.join(__dirname, '../public')
 const viewsPath= path.join(__dirname,'../templates/views')
@@ -96,6 +97,6 @@ app.get('*',(req,res)=>{
 })
 // app.com
 
-app.listen(3000,()=>{
-    console.log('server is up on port 3000.')
+app.listen(port,()=>{
+    console.log('server is up on port '+ port)
 })
